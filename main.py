e36ac6f03413e2beb6678fef73d707827ae91c02
TariqSp <teto75928@gmail.com>
2025-11-11 08:29:41 +0300

    add test method for testing code step by step

--- main.py
+++ main.py
@@ -5,20 +5,8 @@ def print_board(grid):
     for row in grid:
         print(" ".join(row))
     print()
-    
-def main():
-    level = int(input("please write the level you need to play: "))
-    board = Board(level)
 
-    board_info = {
-        "grid": board.grid,
-        "rows": board.row,
-        "cols": board.col,
-    }
-
-    rows = board_info["rows"]
-    cols = board_info["cols"]
-
+def test(board):
     m = Move(board)
     print_board(board.grid)
     print(f"Player positions: {board.player_positions or 'not found'}")
@@ -35,6 +23,24 @@ def main():
     # print(f"Lava cells: {board.lava_positions or 'none'}")
     # print(f"Aqua cells: {board.aqua_positions or 'none'}")
     # print(f"Box cells: {board.box_positions or 'none'}")
+
+    
+def main():
+    level = int(input("please write the level you need to play: "))
+    board = Board(level)
+
+    board_info = {
+        "grid": board.grid,
+        "rows": board.row,
+        "cols": board.col,
+    }
+
+    rows = board_info["rows"]
+    cols = board_info["cols"]
+    
+    test(board)
+
+    
 
 if __name__ == '__main__':
     main()
